b406b17160969eacd8dd7d9d9ece9046ab5a9b09
John <johnconley507@hotmail.com>
2017-02-17 12:39:41 -0500

    adding simple method definitions to class Student

--- CourseAdministration/src/Student.java
+++ CourseAdministration/src/Student.java
@@ -4,5 +4,18 @@ class Student {
 	String firstName;
 	String lastName;
 	int birthYear, birthMonth, birthDay;
-
+	
+	boolean isBirthday() {
+		//Return true if it's the student's birthday today.
+		return false;
+	}
+	
+	void giveWarning(boolean isFinalWarning) {
+		//You should study harder!
+	}
+	
+	int numberOfFriends() {
+		//Return the number of friends the student has.
+		return 0;
+	}
 }
