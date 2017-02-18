f173d2b3bef05df9b1a7fb65c79d71794a2f94be
John <johnconley507@hotmail.com>
2017-02-18 17:55:19 -0500

    creating switch statement

--- RandomJavaPractice/src/SwitchClass.java
+++ RandomJavaPractice/src/SwitchClass.java
@@ -2,7 +2,23 @@
 public class SwitchClass {
 
 	public static void main(String[] args) {
-
+		String loanType = "Commercial";
+		double interestRate;
+		
+		switch(loanType) {
+		
+		case "Residential":
+			interestRate = 0.055;
+			break;
+		case "Commercial":
+			interestRate = 0.062;
+			break;
+		case "Investment":
+			interestRate = 0.059;
+			break;
+		default:
+			interestRate = 0;
+		}
 	}
 
 }
